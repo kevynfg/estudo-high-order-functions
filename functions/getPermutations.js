d4e92ec9af21279018a48d8362515f17eedc8fcb
kevyn <kevynredlotus@gmail.com>
2020-12-10 00:20:20 -0300

    - permutation done

--- functions/getPermutations.js
+++ functions/getPermutations.js
@@ -33,22 +33,101 @@ const getPermutations = (arr) => {
 
 //console.log(getPermutations([1, 2, 3, 4, 5]));
 
-var permArr = [],
-  usedChars = [];
+//console.log(permute([1, 5, 7, 11, 14]));
 
-function permute(input) {
-  let i, ch;
-  for (i = 0; i < input.length; i++) {
-    ch = input.splice(i, 1)[0];
-    usedChars.push(ch);
-    if (input.length == 0) {
-      permArr.push(usedChars.slice());
-    }
-    permute(input);
-    input.splice(i, 0, ch);
-    usedChars.pop();
+//remover itens duplicados
+var numbers = [1, 1, 7, 11, 14];
+let uniqueNames = numbers.filter(
+  (item, pos, self) => self.indexOf(item) == pos
+);
+
+//console.log(uniqueNames);
+
+const gameNumbersSorted = [1, 3, 10, 11, 14, 17, 18, 19, 20, 21, 23, 25];
+const LotteryNumbers = [1, 3, 10, 11, 14];
+let pickedNumbers = 10;
+const selectedGameNumbers = [];
+
+var permute = function (nums, set = [], answers = []) {
+  if (!nums.length) answers.push([...set]);
+
+  for (let i = 0; i < nums.length; i++) {
+    const newNums = nums.filter((n, index) => index !== i);
+    set.push(nums[i]);
+    permute(newNums, set, answers);
+    set.pop();
   }
-  return permArr;
+  return answers;
+};
+
+console.table(permute(LotteryNumbers));
+
+function começar() {
+  let RandomizedArray = [];
+  const moreGamesArray = (number) => {
+    if (number) {
+      RandomizedArray = gameNumbersSorted.sort(() => {
+        return 0.5 - Math.random();
+      });
+    }
+    const item = RandomizedArray.slice(gameNumbersSorted, number).sort(
+      (a, b) => a - b
+    );
+
+    const index = selectedGameNumbers.indexOf(item);
+    if (index > -1) {
+      selectedGameNumbers.splice(index, 1);
+      console.table(selectedGameNumbers);
+    } else {
+      selectedGameNumbers.push(...selectedGameNumbers, item);
+    }
+  };
+  moreGamesArray(4);
 }
 
-console.log(permute([1, 5, 7, 11, 14]));
+function recursivo(count) {
+  let i = 0;
+  if (count) {
+    do {
+      começar();
+      i++;
+    } while (i < count);
+  }
+}
+
+recursivo(5);
+
+function removeItemOnce(arr, value) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+function removeItemAll(arr, value) {
+  var i = 0;
+  while (i < arr.length) {
+    if (arr[i] === value) {
+      arr.splice(i, 1);
+    } else {
+      ++i;
+    }
+  }
+  return arr;
+}
+//Usage
+//console.log(removeItemOnce([2, 5, 9, 1, 5, 8, 5], 5));
+//console.log(removeItemAll([2, 5, 9, 1, 5, 8, 5], 5));
+
+const array = [2, 5, 9];
+
+//console.log(array);
+
+const index = array.indexOf(5);
+if (index > -1) {
+  array.splice(index, 1);
+}
+
+// array = [2, 9]
+//console.log(array);
